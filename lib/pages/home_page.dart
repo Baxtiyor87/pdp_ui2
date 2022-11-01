36d17d9bc0adc859c6e19487ad891cb8f050918b
BMN1987 <110631069+Baxtiyor87@users.noreply.github.com>
2022-11-01 06:58:33 +0500

    1

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,8 +9,42 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final List<String> _listItem =[
+    'assets/images/apple_11.webp',
+    'assets/images/apple_9.jpg',
+    'assets/images/apple_7.png',
+    'assets/images/apple_6.jpg',
+    'assets/images/apple_11.webp',
+    'assets/images/apple_9.jpg',
+    'assets/images/apple_7.png',
+    'assets/images/apple_6.jpg',
+  ];
+  
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.deepOrange,
+      appBar: AppBar(
+        leading: const Icon(Icons.menu),
+        title: const Text('Apple Products'),
+        backgroundColor: Colors.deepOrange,
+        centerTitle: true,
+        actions:  [
+          Padding(
+              padding: EdgeInsets.all(10),
+          child: Container(
+            width: 36,
+            height: 30,
+            decoration: BoxDecoration(
+              color: Colors.orange,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: const Center(
+              child: Text('8', style: TextStyle(fontWeight: FontWeight.bold),),
+            ),
+          ),)
+        ],
+      ),
+    );
   }
 }
